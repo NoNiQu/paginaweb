1312eb9252d17197ff9ba1394243fba4d9e01894
NoNiQu <marcos.workstation@gmail.com>
2025-10-02 17:44:46 +0200

    nueva configuracion de vite

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,7 @@
+import { vercelPreset } from "@vercel/react-router/vite";
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: false,
+  ssr: false,              // SPA (solo cliente)
+  presets: [vercelPreset()] // elimina el WARN y activa integración Vercel
 } satisfies Config;
